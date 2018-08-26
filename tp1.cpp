9929927591e7eadbbd431bbf917fdc9ed8bb046a
Ilan Olkies <ilanolkies@outlook.com>
2018-08-25 21:29:46 -0300

    Read input file

--- tp1.cpp
+++ tp1.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char* argv[]) {
+using namespace std;
+
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        std::cout << "El programa debe cumplir el siguiente formato: ./tp1 archivo p" << std::endl;
+        cout << "El programa debe cumplir el siguiente formato: ./tp1 archivo p" << std::endl;
         return 1;
     }
+
+    // Execution parameters
+    string input_file = argv[1];
+    float p = atof(argv[2]);
+
+    // Read input file
+    ifstream file(input_file);
+
+
+    if (!file.is_open()) {
+        cout << "No ha sido posible leer el archivo " << input_file << "\n";
+        return 1;
+    }
+
+    string line;
+
+    getline(file, line);
+    int n = stoi(line);
+
+    getline(file, line);
+    int m = stoi(line);
+
+    while (getline(file, line))
+        cout << line;
+
+    file.close();
 
     return 0;
 }
